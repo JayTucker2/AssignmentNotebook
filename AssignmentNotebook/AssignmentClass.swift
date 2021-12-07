f834504b16c03735e05558f6b0663a2f4d1f6dce
Mateo Acosta <mateo.acosta22@d155.org>
2021-12-07 08:06:13 -0600

    yesss

---
+++ AssignmentNotebook/AssignmentClass.swift
@@ -0,0 +1,20 @@
+//
+//  AssignmentClass.swift
+//  AssignmentNotebook
+//
+//  Created by mateoacosta on 12/7/21.
+//
+
+import Foundation
+
+class AssigbnmentClass{
+var className = ""
+var assignmentName = ""
+var dueDate = ""
+var isDone = false
+    
+    AssigbnmentClass(){
+        
+    }
+    
+}
